d3104a4f4c1cdaa16af465506390dfa102f4e82a
Eric Lasota <ejlasota@gmail.com>
2015-04-11 21:34:15 -0400

    Deploy adjustments.

--- makerelease.bat
+++ makerelease.bat
@@ -1,39 +1,48 @@
-rmdir /S /Q deploy
-mkdir deploy
-mkdir deploy\external-sources
-mkdir deploy\bin
-mkdir deploy\bin\ffmpeg
-mkdir deploy\lib
-mkdir deploy\include
-mkdir deploy\include\lwmovie
-mkdir deploy\include\common
+rmdir /S /Q lwmovie-release
+mkdir lwmovie-release
+mkdir lwmovie-release\external-sources
+mkdir lwmovie-release\bin
+mkdir lwmovie-release\bin\ffmpeg
+mkdir lwmovie-release\lib
+mkdir lwmovie-release\include
+mkdir lwmovie-release\include\lwmovie
+mkdir lwmovie-release\include\common
+mkdir lwmovie-release\doc
 call "D:\Low Speed Installs\Microsoft Visual Studio 10.0\VC\vcvarsall.bat"
 msbuild.exe lwmovie.sln /t:Rebuild /p:Configuration=Release
 msbuild.exe lwfe.sln /t:Rebuild /p:Configuration=Release
 
-copy /Y Release\lwmovie.lib deploy\lib\lwmovie.lib
+copy /Y Release\lwmovie.lib lwmovie-release\lib\lwmovie.lib
 
-copy /Y lwmovie\lwmovie.h deploy\include\lwmovie\lwmovie.h
-copy /Y lwmovie\lwmovie_cake.h deploy\include\lwmovie\lwmovie_cake.h
-copy /Y lwmovie\lwmovie_cake_cppshims.h deploy\include\lwmovie\lwmovie_cake_cppshims.h
-copy /Y lwmovie\lwmovie_cpp_shims.hpp deploy\include\lwmovie\lwmovie_cpp_shims.hpp
-copy /Y lwmovie\lwmovie_api.h deploy\include\lwmovie\lwmovie_api.h
-copy /Y lwmovie\lwmovie_external_types.h deploy\include\lwmovie\lwmovie_external_types.h
-copy /Y lwmovie\lwmovie_common.h deploy\include\lwmovie\lwmovie_common.h
-copy /Y lwmovie\lwmovie_attribs.h deploy\include\lwmovie\lwmovie_attribs.h
+copy /Y lwmovie\lwmovie.h lwmovie-release\include\lwmovie\lwmovie.h
+copy /Y lwmovie\lwmovie_cake.h lwmovie-release\include\lwmovie\lwmovie_cake.h
+copy /Y lwmovie\lwmovie_cake_cppshims.h lwmovie-release\include\lwmovie\lwmovie_cake_cppshims.h
+copy /Y lwmovie\lwmovie_cpp_shims.hpp lwmovie-release\include\lwmovie\lwmovie_cpp_shims.hpp
+copy /Y lwmovie\lwmovie_api.h lwmovie-release\include\lwmovie\lwmovie_api.h
+copy /Y lwmovie\lwmovie_external_types.h lwmovie-release\include\lwmovie\lwmovie_external_types.h
+copy /Y lwmovie\lwmovie_common.h lwmovie-release\include\lwmovie\lwmovie_common.h
+copy /Y lwmovie\lwmovie_attribs.h lwmovie-release\include\lwmovie\lwmovie_attribs.h
 
-copy /Y Release\lwmovie.dll deploy\bin\lwmovie.dll
-copy /Y Release\lwenctools.dll deploy\bin\lwenctools.dll
-copy /Y Release\lwplay.exe deploy\bin\lwplay.exe
-copy /Y Release\lwrerange.exe deploy\bin\lwrerange.exe
-copy /Y Release\lwmux.exe deploy\bin\lwmux.exe
-copy /Y Release\lwfe.exe deploy\bin\lwfe.exe
-copy /Y Release\SDL2.dll deploy\bin\SDL2.dll
-xcopy /Y /E Release\ffmpeg deploy\bin\ffmpeg
-xcopy /Y /E external-sources deploy\external-sources
+copy /Y Release\lwmovie.dll lwmovie-release\bin\lwmovie.dll
+copy /Y Release\lwenctools.dll lwmovie-release\bin\lwenctools.dll
+copy /Y Release\lwplay.exe lwmovie-release\bin\lwplay.exe
+copy /Y Release\lwrerange.exe lwmovie-release\bin\lwrerange.exe
+copy /Y Release\lwmux.exe lwmovie-release\bin\lwmux.exe
+copy /Y Release\lwfe.exe lwmovie-release\bin\lwfe.exe
+copy /Y Release\SDL2.dll lwmovie-release\bin\SDL2.dll
+
+copy /Y CommandLineOptions.txt lwmovie-release\doc\
+copy /Y Cake_Documentation.pdf lwmovie-release\doc\
+copy /Y architecture.pdf lwmovie-release\doc\
+copy /Y LICENSE.txt lwmovie-release\doc\
+copy /Y README-libjpeg.txt lwmovie-release\doc\
+copy /Y Using_lwmovie.txt lwmovie-release\doc\
+
+xcopy /Y /E Release\ffmpeg lwmovie-release\bin\ffmpeg
+xcopy /Y /E external-sources lwmovie-release\external-sources
 
 del deploy.7z
-"C:\Program Files\7-zip\7z.exe" a -mx=9 deploy.7z deploy
-rmdir /S /Q deploy
+"C:\Program Files\7-zip\7z.exe" a -mx=9 deploy.7z lwmovie-release
+rmdir /S /Q lwmovie-release
 pause
 
