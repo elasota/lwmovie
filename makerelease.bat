d56b0512b99b70f522d49a08833d99315d061ade
Eric Lasota <ejlasota@gmail.com>
2015-04-14 02:38:35 -0400

    Deploy fixups.

--- makerelease.bat
+++ makerelease.bat
@@ -8,22 +8,26 @@ mkdir lwmovie-release\include
 mkdir lwmovie-release\include\lwmovie
 mkdir lwmovie-release\include\common
 mkdir lwmovie-release\doc
-call "D:\Low Speed Installs\Microsoft Visual Studio 10.0\VC\vcvarsall.bat"
-msbuild.exe lwmovie.sln /t:Rebuild /p:Configuration=Release
+call "D:\Low Speed Installs\Microsoft Visual Studio 12.0\VC\vcvarsall.bat"
+msbuild.exe lwmovie.sln /t:Rebuild /p:Configuration=Release /p:Platform=x64
+msbuild.exe lwmovie.sln /t:Rebuild /p:Configuration=Release /p:Platform=Win32
 msbuild.exe lwfe.sln /t:Rebuild /p:Configuration=Release
 
 copy /Y Release\lwmovie.lib lwmovie-release\lib\lwmovie.lib
+copy /Y x64\Release\lwmovie64.lib lwmovie-release\lib\lwmovie64.lib
 
 copy /Y lwmovie\lwmovie.h lwmovie-release\include\lwmovie\lwmovie.h
 copy /Y lwmovie\lwmovie_cake.h lwmovie-release\include\lwmovie\lwmovie_cake.h
-copy /Y lwmovie\lwmovie_cake_cppshims.h lwmovie-release\include\lwmovie\lwmovie_cake_cppshims.h
+copy /Y lwmovie\lwmovie_cake_cppshims.hpp lwmovie-release\include\lwmovie\lwmovie_cake_cppshims.hpp
 copy /Y lwmovie\lwmovie_cpp_shims.hpp lwmovie-release\include\lwmovie\lwmovie_cpp_shims.hpp
 copy /Y lwmovie\lwmovie_api.h lwmovie-release\include\lwmovie\lwmovie_api.h
 copy /Y lwmovie\lwmovie_external_types.h lwmovie-release\include\lwmovie\lwmovie_external_types.h
-copy /Y lwmovie\lwmovie_common.h lwmovie-release\include\lwmovie\lwmovie_common.h
-copy /Y lwmovie\lwmovie_attribs.h lwmovie-release\include\lwmovie\lwmovie_attribs.h
+copy /Y common\lwmovie_config.h lwmovie-release\include\common\lwmovie_common.h
+copy /Y common\lwmovie_attribs.h lwmovie-release\include\common\lwmovie_attribs.h
+copy /Y common\lwmovie_coretypes.h lwmovie-release\include\common\lwmovie_coretypes.h
 
 copy /Y Release\lwmovie.dll lwmovie-release\bin\lwmovie.dll
+copy /Y x64\Release\lwmovie64.dll lwmovie-release\bin\lwmovie64.dll
 copy /Y Release\lwenctools.dll lwmovie-release\bin\lwenctools.dll
 copy /Y Release\lwplay.exe lwmovie-release\bin\lwplay.exe
 copy /Y Release\lwrerange.exe lwmovie-release\bin\lwrerange.exe
