d244f6b98853e9ddb6a1e00d25ce120f73f79fdf
Eric Lasota <ejlasota@gmail.com>
2015-04-20 20:44:16 -0400

    Fixed Coverity build.

--- coverity-build.bat
+++ coverity-build.bat
@@ -1,4 +1,4 @@
-call "D:\Low Speed Installs\Microsoft Visual Studio 10.0\VC\vcvarsall.bat"
+call "D:\Low Speed Installs\Microsoft Visual Studio 12.0\VC\vcvarsall.bat"
 "D:\Source Code\cov-analysis-win64-7.6.0\bin\cov-build.exe" --dir cov-int msbuild.exe lwmovie.sln /t:Rebuild /p:Configuration=Release
 "C:\Program Files\7-zip\7z.exe" a lwmovie-cov.tar cov-int
 "C:\Program Files\7-zip\7z.exe" a -mx=9 lwmovie-cov.tar.gz lwmovie-cov.tar
